b7ad12227a4cd15ab21692075690b1532ca17070
LDanaee <kercyan@live.fr>
2017-09-17 21:30:06 +0200

    Part 7
    Function for bars
    Function  for game messages
    Mouse over a monster get the name

--- engine.py
+++ engine.py
@@ -1,12 +1,13 @@
 import libtcodpy as libtcod
 from input_handlers import handle_keys
 from entity import Entity, get_blocking_entities_at_location
-from render_function import render_all, clear_all, RenderOrder
+from render_function import render_all, clear_all, RenderOrder, render_bar
 from map_objects.game_map import GameMap
 from fov_functions import initialize_fov, recompute_fov
 from game_states import GameStates
 from components.fighter import Fighter
 from components.death_function import kill_player, kill_monster
+from game_messages import MessageLog, Message
 
 #fonction main Check if the module is ran as main program (name devient main).
 #Si ce fichier est importé d'un autre module, name sera le nom du module
@@ -15,7 +16,17 @@ def main():
     screen_width = 80
     screen_height = 50
     map_width = 80
-    map_height = 45
+    map_height = 43
+
+#Carac des barres
+    bar_width = 20
+    panel_height = 7
+    panel_y = screen_height - panel_height
+#Message box
+    message_x = bar_width + 2
+    message_width = screen_width - bar_width - 2
+    message_height = panel_height - 1
+
 #Minimum de la room
     room_max_size = 15
     room_min_size = 6
@@ -29,10 +40,10 @@ def main():
 
 
     colors = {
-        'dark_wall': libtcod.Color(128, 128, 128),
-        'dark_ground': libtcod.Color(0, 0, 0),
-        'light_wall': libtcod.Color(130, 110, 50),
-        'light_ground': libtcod.Color(200, 180, 50)
+        'dark_wall': libtcod.Color(51, 0, 25),
+        'dark_ground': libtcod.Color(0, 51, 51),
+        'light_wall': libtcod.Color(255, 0, 127),
+        'light_ground': libtcod.Color(0, 153, 153)
     }
 
     fighter_component = Fighter(hp=30, defense=2, power=5)
@@ -45,12 +56,15 @@ def main():
     libtcod.console_init_root(screen_width, screen_height, 'Eolandia', False)
 #Console principale?
     con = libtcod.console_new(screen_width, screen_height)
+#Message box
+    panel = libtcod.console_new(screen_width, panel_height)
 #Initialise la Game map
     game_map = GameMap(map_width, map_height)
     game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities, max_monsters_per_room)
 
     fov_recompute = True
     fov_map = initialize_fov(game_map)
+    message_log = MessageLog(message_x, message_width, message_height)
 
 
     key = libtcod.Key()
@@ -60,12 +74,13 @@ def main():
 #boucle qui se finit pas
     while not libtcod.console_is_window_closed():
 #Function that capture new event
-        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS | libtcod.EVENT_MOUSE, key, mouse)
 
         if fov_recompute:
             recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
 #Function that draws entities
-        render_all(con, entities, player, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
+        render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, screen_width,
+                   screen_height, bar_width, panel_height, panel_y, mouse, colors)
 
         fov_recompute = False
 
@@ -111,7 +126,7 @@ def main():
             dead_entity = player_turn_result.get('dead')
 
             if message:
-                print(message)
+                message_log.add_message(message)
 
             if dead_entity:
                 if dead_entity == player:
@@ -119,7 +134,7 @@ def main():
                 else:
                     message = kill_monster(dead_entity)
 
-                print(message)
+                message_log.add_message(message)
 
         if game_state == GameStates.ENEMY_TURN:
             for entity in entities:
@@ -131,7 +146,7 @@ def main():
                         dead_entity = enemy_turn_result.get('dead')
 
                         if message:
-                            print(message)
+                            message_log.add_message(message)
 
                         if dead_entity:
                             if dead_entity == player:
@@ -139,7 +154,7 @@ def main():
                             else:
                                 message = kill_monster(dead_entity)
 
-                            print(message)
+                            message_log.add_message(message)
 
                             if game_state == GameStates.PLAYER_DEAD:
                                 break
