60f701b8b2b90f2e87f8f4cd05e7e919a3b971b1
LDanaee <kercyan@live.fr>
2018-08-10 16:19:44 +0200

    i don''t remember what i did update

--- render_function.py
+++ render_function.py
@@ -2,10 +2,12 @@ import libtcodpy as libtcod
 from enum import Enum
 from components.ui_draw import draw_window_borders
 
+
 class RenderOrder(Enum):
     CORPSE = 1
     ITEM = 2
     ACTOR = 3
+
 
 def get_names_under_mouse(mouse, entities, fov_map):
     (x, y) = (mouse.cx, mouse.cy)
@@ -15,6 +17,7 @@ def get_names_under_mouse(mouse, entities, fov_map):
     names = ', '.join(names)
 
     return names.capitalize()
+
 
 def render_bar(panel, x, y, total_width, name, value, maximum, bar_color, back_color):
     bar_width = int(float(value) / maximum * total_width)
@@ -29,6 +32,7 @@ def render_bar(panel, x, y, total_width, name, value, maximum, bar_color, back_c
     libtcod.console_set_default_foreground(panel, libtcod.white)
     libtcod.console_print_ex(panel, int(x + total_width / 2), y, libtcod.BKGND_NONE, libtcod.CENTER,
                              '{0}: {1}/{2}'.format(name, value, maximum))
+
 
 def render_all(con, panel, card_panel, entities, player, game_map, fov_map, fov_recompute, message_log, screen_width, screen_height, bar_width, panel_height, panel_y, card_panel_height, card_panel_y, mouse, colors):
     if fov_recompute:
@@ -46,14 +50,14 @@ def render_all(con, panel, card_panel, entities, player, game_map, fov_map, fov_
                 elif game_map.tiles[x][y].explored:
                     if wall:
                         libtcod.console_set_char_background(con, x, y, colors.get('dark_wall'), libtcod.BKGND_SET)
-                    else:#if not draw floor
+                    else:# if not draw floor
                         libtcod.console_set_char_background(con, x, y, colors.get('dark_ground'), libtcod.BKGND_SET)
 
     # Draw all entities in the list
     entities_in_render_order = sorted(entities, key=lambda x: x.render_order.value)
     for entity in entities_in_render_order:
         draw_entity(con, entity, fov_map)
-#blits???
+# blits???
     libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
 
     libtcod.console_set_default_background(panel, libtcod.black)
@@ -69,7 +73,7 @@ def render_all(con, panel, card_panel, entities, player, game_map, fov_map, fov_
         y += 1
 
     render_bar(panel, 1, 1, bar_width, 'HP', player.fighter.hp,
-    player.fighter.max_hp, libtcod.light_red, libtcod.darker_red)
+               player.fighter.max_hp, libtcod.light_red, libtcod.darker_red)
     libtcod.console_set_default_foreground(panel, libtcod.light_gray)
     libtcod.console_print_ex(panel, 1, 0, libtcod.BKGND_NONE, libtcod.LEFT, get_names_under_mouse(mouse, entities, fov_map))
 
@@ -81,7 +85,9 @@ def clear_all(con, entities):
     for entity in entities:
         clear_entity(con, entity)
 
-#function qui draw duh
+# function qui draw duh
+
+
 def draw_entity(con, entity, fov_map):
     if libtcod.map_is_in_fov(fov_map, entity.x, entity.y):
         libtcod.console_set_default_foreground(con, entity.color)
